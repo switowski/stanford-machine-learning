794791105aa50271f844a55dd42e232c1705b776
Sebastian Witowski <witowski.sebastian@gmail.com>
2018-10-04 20:11:12 +0200

    as5: learning curves

--- ex5/ex5/ex5.m
+++ ex5/ex5/ex5.m
@@ -3,7 +3,7 @@
 %
 %  Instructions
 %  ------------
-% 
+%
 %  This file contains code that helps you get started on the
 %  exercise. You will need to complete the following functions:
 %
@@ -19,7 +19,7 @@
 clear ; close all; clc
 
 %% =========== Part 1: Loading and Visualizing Data =============
-%  We start the exercise by first loading and visualizing the dataset. 
+%  We start the exercise by first loading and visualizing the dataset.
 %  The following code will load the dataset into your environment and plot
 %  the data.
 %
@@ -27,81 +27,81 @@ clear ; close all; clc
 % Load Training Data
 fprintf('Loading and Visualizing Data ...\n')
 
-% Load from ex5data1: 
+% Load from ex5data1:
 % You will have X, y, Xval, yval, Xtest, ytest in your environment
 load ('ex5data1.mat');
 
 % m = Number of examples
 m = size(X, 1);
 
-% Plot training data
-plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
-xlabel('Change in water level (x)');
-ylabel('Water flowing out of the dam (y)');
+% % Plot training data
+% plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+% xlabel('Change in water level (x)');
+% ylabel('Water flowing out of the dam (y)');
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
 
 %% =========== Part 2: Regularized Linear Regression Cost =============
-%  You should now implement the cost function for regularized linear 
-%  regression. 
-%
-
-theta = [1 ; 1];
-J = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
-
-fprintf(['Cost at theta = [1 ; 1]: %f '...
-         '\n(this value should be about 303.993192)\n'], J);
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
-%% =========== Part 3: Regularized Linear Regression Gradient =============
-%  You should now implement the gradient for regularized linear 
+%  You should now implement the cost function for regularized linear
 %  regression.
 %
 
-theta = [1 ; 1];
-[J, grad] = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
+% theta = [1 ; 1];
+% J = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
 
-fprintf(['Gradient at theta = [1 ; 1]:  [%f; %f] '...
-         '\n(this value should be about [-15.303016; 598.250744])\n'], ...
-         grad(1), grad(2));
+% fprintf(['Cost at theta = [1 ; 1]: %f '...
+%          '\n(this value should be about 303.993192)\n'], J);
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
+
+%% =========== Part 3: Regularized Linear Regression Gradient =============
+%  You should now implement the gradient for regularized linear
+%  regression.
+%
+
+% theta = [1 ; 1];
+% [J, grad] = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
+
+% fprintf(['Gradient at theta = [1 ; 1]:  [%f; %f] '...
+%          '\n(this value should be about [-15.303016; 598.250744])\n'], ...
+%          grad(1), grad(2));
+
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
 
 
 %% =========== Part 4: Train Linear Regression =============
 %  Once you have implemented the cost and gradient correctly, the
-%  trainLinearReg function will use your cost function to train 
+%  trainLinearReg function will use your cost function to train
 %  regularized linear regression.
-% 
-%  Write Up Note: The data is non-linear, so this will not give a great 
+%
+%  Write Up Note: The data is non-linear, so this will not give a great
 %                 fit.
 %
 
 %  Train linear regression with lambda = 0
 lambda = 0;
-[theta] = trainLinearReg([ones(m, 1) X], y, lambda);
+% [theta] = trainLinearReg([ones(m, 1) X], y, lambda);
 
-%  Plot fit over the data
-plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
-xlabel('Change in water level (x)');
-ylabel('Water flowing out of the dam (y)');
-hold on;
-plot(X, [ones(m, 1) X]*theta, '--', 'LineWidth', 2)
-hold off;
+% %  Plot fit over the data
+% plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+% xlabel('Change in water level (x)');
+% ylabel('Water flowing out of the dam (y)');
+% hold on;
+% plot(X, [ones(m, 1) X]*theta, '--', 'LineWidth', 2)
+% hold off;
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
 
 
 %% =========== Part 5: Learning Curve for Linear Regression =============
-%  Next, you should implement the learningCurve function. 
+%  Next, you should implement the learningCurve function.
 %
 %  Write Up Note: Since the model is underfitting the data, we expect to
-%                 see a graph with "high bias" -- Figure 3 in ex5.pdf 
+%                 see a graph with "high bias" -- Figure 3 in ex5.pdf
 %
 
 lambda = 0;
@@ -159,7 +159,7 @@ pause;
 
 %% =========== Part 7: Learning Curve for Polynomial Regression =============
 %  Now, you will get to experiment with polynomial regression with multiple
-%  values of lambda. The code below runs polynomial regression with 
+%  values of lambda. The code below runs polynomial regression with
 %  lambda = 0. You should try running the code with different values of
 %  lambda to see how the fit and learning curve change.
 %
@@ -196,7 +196,7 @@ fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 %% =========== Part 8: Validation for Selecting Lambda =============
-%  You will now implement validationCurve to test various values of 
+%  You will now implement validationCurve to test various values of
 %  lambda on a validation set. You will then use this to select the
 %  "best" lambda value.
 %
