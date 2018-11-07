617718530b989f29c99ce6befb570a799a2dfd3b
Sebastian Witowski <witowski.sebastian@gmail.com>
2018-11-07 17:08:02 +0100

    as7 done

--- ex7/ex7/ex7_pca.m
+++ ex7/ex7/ex7_pca.m
@@ -28,12 +28,12 @@ clear ; close all; clc
 % fprintf('Visualizing example dataset for PCA.\n\n');
 
 %  The following command loads the dataset. You should now have the
-%  variable X in your environment
-load ('ex7data1.mat');
+% %  variable X in your environment
+% load ('ex7data1.mat');
 
-%  Visualize the example dataset
-plot(X(:, 1), X(:, 2), 'bo');
-axis([0.5 6.5 2 8]); axis square;
+% %  Visualize the example dataset
+% plot(X(:, 1), X(:, 2), 'bo');
+% axis([0.5 6.5 2 8]); axis square;
 
 % fprintf('Program paused. Press enter to continue.\n');
 % pause;
@@ -43,29 +43,29 @@ axis([0.5 6.5 2 8]); axis square;
 %  You should now implement PCA, a dimension reduction technique. You
 %  should complete the code in pca.m
 %
-fprintf('\nRunning PCA on example dataset.\n\n');
+% fprintf('\nRunning PCA on example dataset.\n\n');
 
-%  Before running PCA, it is important to first normalize X
-[X_norm, mu, sigma] = featureNormalize(X);
+% %  Before running PCA, it is important to first normalize X
+% [X_norm, mu, sigma] = featureNormalize(X);
 
-%  Run PCA
-[U, S] = pca(X_norm);
+% %  Run PCA
+% [U, S] = pca(X_norm);
 
 %  Compute mu, the mean of the each feature
 
-%  Draw the eigenvectors centered at mean of data. These lines show the
-%  directions of maximum variations in the dataset.
-hold on;
-drawLine(mu, mu + 1.5 * S(1,1) * U(:,1)', '-k', 'LineWidth', 2);
-drawLine(mu, mu + 1.5 * S(2,2) * U(:,2)', '-k', 'LineWidth', 2);
-hold off;
+% %  Draw the eigenvectors centered at mean of data. These lines show the
+% %  directions of maximum variations in the dataset.
+% hold on;
+% drawLine(mu, mu + 1.5 * S(1,1) * U(:,1)', '-k', 'LineWidth', 2);
+% drawLine(mu, mu + 1.5 * S(2,2) * U(:,2)', '-k', 'LineWidth', 2);
+% hold off;
 
-fprintf('Top eigenvector: \n');
-fprintf(' U(:,1) = %f %f \n', U(1,1), U(2,1));
-fprintf('\n(you should expect to see -0.707107 -0.707107)\n');
+% fprintf('Top eigenvector: \n');
+% fprintf(' U(:,1) = %f %f \n', U(1,1), U(2,1));
+% fprintf('\n(you should expect to see -0.707107 -0.707107)\n');
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
 
 
 %% =================== Part 3: Dimension Reduction ===================
@@ -76,32 +76,32 @@ pause;
 %
 %  You should complete the code in projectData.m
 %
-fprintf('\nDimension reduction on example dataset.\n\n');
+% fprintf('\nDimension reduction on example dataset.\n\n');
 
-%  Plot the normalized dataset (returned from pca)
-plot(X_norm(:, 1), X_norm(:, 2), 'bo');
-axis([-4 3 -4 3]); axis square
+% %  Plot the normalized dataset (returned from pca)
+% plot(X_norm(:, 1), X_norm(:, 2), 'bo');
+% axis([-4 3 -4 3]); axis square
 
-%  Project the data onto K = 1 dimension
-K = 1;
-Z = projectData(X_norm, U, K);
-fprintf('Projection of the first example: %f\n', Z(1));
-fprintf('\n(this value should be about 1.481274)\n\n');
+% %  Project the data onto K = 1 dimension
+% K = 1;
+% Z = projectData(X_norm, U, K);
+% fprintf('Projection of the first example: %f\n', Z(1));
+% fprintf('\n(this value should be about 1.481274)\n\n');
 
-X_rec  = recoverData(Z, U, K);
-fprintf('Approximation of the first example: %f %f\n', X_rec(1, 1), X_rec(1, 2));
-fprintf('\n(this value should be about  -1.047419 -1.047419)\n\n');
+% X_rec  = recoverData(Z, U, K);
+% fprintf('Approximation of the first example: %f %f\n', X_rec(1, 1), X_rec(1, 2));
+% fprintf('\n(this value should be about  -1.047419 -1.047419)\n\n');
 
-%  Draw lines connecting the projected points to the original points
-hold on;
-plot(X_rec(:, 1), X_rec(:, 2), 'ro');
-for i = 1:size(X_norm, 1)
-    drawLine(X_norm(i,:), X_rec(i,:), '--k', 'LineWidth', 1);
-end
-hold off
+% %  Draw lines connecting the projected points to the original points
+% hold on;
+% plot(X_rec(:, 1), X_rec(:, 2), 'ro');
+% for i = 1:size(X_norm, 1)
+%     drawLine(X_norm(i,:), X_rec(i,:), '--k', 'LineWidth', 1);
+% end
+% hold off
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
 
 %% =============== Part 4: Loading and Visualizing Face Data =============
 %  We start the exercise by first loading and visualizing the dataset.
